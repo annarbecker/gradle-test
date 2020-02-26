a5118a0cad31789778dc9a66e79534816b675d3f
Anna Becker <anna.becker@urbanairship.com>
2020-02-26 15:15:47 -0800

    Update test

--- src/test/java/TestGradle.java
+++ src/test/java/TestGradle.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TestGradle {
@@ -7,5 +8,6 @@ public class TestGradle {
     @Test
     public void testGradle() {
         assertTrue(2 + 2 == 4);
+        assertFalse(2+2 == 5);
     }
 }
