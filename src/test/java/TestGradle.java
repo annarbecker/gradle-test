107d02b75a80008f8cb454df69f2ce381ed40b1d
Anna Becker <anna.becker@urbanairship.com>
2020-02-26 15:08:46 -0800

    Add test

---
+++ src/test/java/TestGradle.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestGradle {
+
+    @Test
+    public void testGradle() {
+        assertTrue(2 + 2 == 4);
+    }
+}
